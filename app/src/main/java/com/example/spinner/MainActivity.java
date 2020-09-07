9ccfee17bab838484db7a2b4052bba151dc64d78
Ganesh Aghav <ganesha@datacomp.co.in>
2020-09-07 23:48:05 +0530

    first commit

---
+++ app/src/main/java/com/example/spinner/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.spinner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    Spinner spinner;
+    String[] country = { "India", "USA", "China", "Japan", "Other"};
+
+    //implementation 'com.android.support:cardview-v7:28.+'
+    //implementation 'com.android.support:recyclerview-v7:28.+'
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        spinner=(Spinner) findViewById(R.id.spinner);
+        spinner.setOnItemSelectedListener(MainActivity.this);
+        ArrayAdapter aa = new ArrayAdapter(this,android.R.layout.simple_spinner_item,country);
+        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(aa);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        Toast.makeText(getApplicationContext(),country[position] , Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+}
